ee9f41d7a3af4272edaa9d4129a9069db69fa039
Ruan <ruan.klein@gmail.com>
2024-10-14 19:50:29 -0300

    chore: movie integration tests added

---
+++ src/tests/integration/movie.integration.spec.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { MovieRepository } from '../../repositories/MovieRepository'
+import { MovieService } from '../../services/MovieService'
+import { Movie } from '../../models/Movie'
+import { initializeDatabase } from '../../database/connection'
+
+const movieMock: Movie = {
+  id: 1,
+  title: 'The Matrix',
+  producers: 'Joel Silver',
+  studios: 'Warner Bros',
+  year: 1999,
+  winner: false,
+}
+
+describe('Movie Service Integration Tests', () => {
+  let movieRepository: MovieRepository
+  let movieService: MovieService
+
+  beforeAll(() => {
+    movieRepository = new MovieRepository()
+    movieService = new MovieService(movieRepository)
+
+    initializeDatabase()
+  })
+
+  it('should create a new movie', async () => {
+    const movie = new Movie(
+      movieMock.id,
+      movieMock.title,
+      movieMock.studios,
+      movieMock.producers,
+      movieMock.year,
+      movieMock.winner
+    )
+
+    const newMovie = await movieService.create(movie)
+
+    expect(newMovie).toBeInstanceOf(Movie)
+    expect(newMovie).toMatchObject(movieMock)
+  })
+
+  it('should return a list of movies', async () => {
+    const movies = await movieService.findAll()
+
+    expect(Array.isArray(movies)).toBe(true)
+    movies.forEach((movie) => {
+      expect(movie).toBeInstanceOf(Movie)
+    })
+  })
+
+  it('should return a movie by id', async () => {
+    const movie = await movieService.findById(1)
+
+    expect(movie).toBeInstanceOf(Movie)
+    expect(movie).toMatchObject(movieMock)
+  })
+
+  it('should update a movie', async () => {
+    let movie = (await movieService.findById(1))!
+
+    movie.title = 'The Matrix Reloaded'
+    movie.year = 2003
+
+    const updatedMovie = await movieService.update(movie)
+    movie = (await movieService.findById(1))!
+
+    expect(updatedMovie).toBeInstanceOf(Movie)
+    expect(updatedMovie).toMatchObject(movie)
+  })
+
+  it('should delete a movie', async () => {
+    const deletedMovie = await movieService.delete(1)
+    const movie = await movieService.findById(1)
+
+    expect(deletedMovie).toBe(1)
+    expect(movie).toBeNull()
+  })
+})
