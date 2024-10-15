be2ba4a13cca27fa65182fb2ca193d8a3134733b
Ruan <ruan.klein@gmail.com>
2024-10-14 21:50:46 -0300

    chore: validations changed

--- src/validators/MovieValidator.ts
+++ src/validators/MovieValidator.ts
@@ -1,30 +1,48 @@
 import { param, body, type ValidationChain } from 'express-validator'
 
 export const showValidationRules = (): ValidationChain[] => {
-  return [param('id').isInt().toInt()]
+  return [param('id').isInt().withMessage('ID must be an integer')]
 }
 
 export const storeValidationRules = (): ValidationChain[] => {
   return [
-    body('title').isString(),
-    body('studios').isString(),
-    body('producers').isString(),
-    body('year').isInt().toInt(),
-    body('winner').isBoolean().toBoolean(),
+    body('title')
+      .isString()
+      .isLength({ min: 1, max: 40 })
+      .withMessage('Title must be between 1 and 40 characters'),
+    body('studios')
+      .isString()
+      .isLength({ min: 1, max: 80 })
+      .withMessage('Studios must be between 1 and 80 characters'),
+    body('producers')
+      .isString()
+      .isLength({ min: 1, max: 80 })
+      .withMessage('Producers must be between 1 and 80 characters'),
+    body('year').isInt().toInt().withMessage('Year must be an integer'),
+    body('winner').isBoolean().withMessage('Winner must be a boolean'),
   ]
 }
 
 export const updateValidationRules = (): ValidationChain[] => {
   return [
-    param('id').isInt().toInt(),
-    body('title').isString(),
-    body('studios').isString(),
-    body('producers').isString(),
-    body('year').isInt().toInt(),
-    body('winner').isBoolean().toBoolean(),
+    param('id').isInt().withMessage('ID must be an integer'),
+    body('title')
+      .isString()
+      .isLength({ min: 1, max: 40 })
+      .withMessage('Title must be between 1 and 40 characters'),
+    body('studios')
+      .isString()
+      .isLength({ min: 1, max: 80 })
+      .withMessage('Studios must be between 1 and 80 characters'),
+    body('producers')
+      .isString()
+      .isLength({ min: 1, max: 80 })
+      .withMessage('Producers must be between 1 and 80 characters'),
+    body('year').isInt().toInt().withMessage('Year must be an integer'),
+    body('winner').isBoolean().withMessage('Winner must be a boolean'),
   ]
 }
 
 export const destroyValidationRules = (): ValidationChain[] => {
-  return [param('id').isInt().toInt()]
+  return [param('id').isInt().withMessage('ID must be an integer')]
 }
